d8b59413131d98cecd7b22eb242a079a415b603f
stray-code <abc1007020@gmail.com>
2024-11-09 07:42:15 +0900

    バージョンを1.0.2に更新

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ const manifest = defineManifest({
   manifest_version: 3,
   name: "サイトの色を確認",
   description: "サイトの色（カラーコード）を知ることができます。",
-  version: "1.0.1",
+  version: "1.0.2",
   icons: {
     16: "img/icon16.png",
     48: "img/icon48.png",
